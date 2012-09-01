27179f65b7fdeac49c6c23dc2997a51bcc30eff4
Francis Galiegue <francis.galiegue@commerceguys.com>
2012-09-01 16:25:38 +0200

    Add tests
    Ripoff from the master project, basically.
    
    Also make the context null in AbstractFormatSpecifierTest: this is an ugly hack,
    due to the fact that TestNG cannot seem to instantiate FormatBundle otherwise...

---
+++ src/test/java/org/eel/kitchen/jsonschema/formats/AbstractFormatSpecifierTest.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2012, Francis Galiegue <fgaliegue@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Lesser GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Lesser GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.eel.kitchen.jsonschema.formats;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import org.eel.kitchen.jsonschema.format.FormatSpecifier;
+import org.eel.kitchen.jsonschema.report.ValidationReport;
+import org.eel.kitchen.jsonschema.util.JsonLoader;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+import static org.testng.Assert.*;
+
+public abstract class AbstractFormatSpecifierTest
+{
+    private final FormatSpecifier specifier;
+    private final String fmt;
+
+    private final JsonNode testData;
+
+    AbstractFormatSpecifierTest(final FormatSpecifier specifier,
+        final String resourceName)
+        throws IOException
+    {
+        this.specifier = specifier;
+        fmt = resourceName;
+
+        testData = JsonLoader.fromResource("/formats/" + resourceName
+            + ".json");
+    }
+
+    @DataProvider
+    protected Iterator<Object[]> getData()
+    {
+        final Set<Object[]> set = new HashSet<Object[]>();
+
+        for (final JsonNode node: testData)
+            set.add(new Object[] {
+                node.get("data"),
+                node.get("valid").booleanValue()
+            }
+            );
+
+        return set.iterator();
+    }
+
+    @Test(dataProvider = "getData", invocationCount = 10, threadPoolSize = 4)
+    public void testSpecifier(final JsonNode data, final boolean valid)
+    {
+        final ValidationReport report = new ValidationReport();
+
+        specifier.checkValue(fmt, null, report, data);
+
+        assertEquals(report.isSuccess(), valid);
+    }
+}
